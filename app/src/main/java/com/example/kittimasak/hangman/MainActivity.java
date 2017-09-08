24e12df73240a27300da4c9fb5a908e7139c5346
Kittimasak Bunrat <kittimasak.bunrat@gmail.com>
2017-09-08 15:02:40 +0200

    - gameLogic with correct counter done ? Missing onsaveInstance

--- app/src/main/java/com/example/kittimasak/hangman/MainActivity.java
+++ app/src/main/java/com/example/kittimasak/hangman/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.kittimasak.hangman;
 
 import android.content.res.Resources;
 import android.graphics.Color;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -31,6 +32,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     String ord;
     final int maxforsok = 6;
     int antallforsok;
+    boolean loop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +47,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         int random = new Random().nextInt(ordliste.size());
         ord = ordliste.get(random);
         antallforsok = 0;
+        loop = true;
 
         setArray(ordArray, ord);
 
@@ -68,6 +71,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+    }
+
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+    }
+
+    public void counter(boolean b) {
+        if(b) {
+            antallforsok++;
+        }
+    }
+
 
     public void setArray(ArrayList<Character> ord,String s) {
         String k = s.toUpperCase();
@@ -77,16 +97,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     public void gameLogic(ArrayList<Character> ord, ArrayList<Character> temp, Character c, Button b) {
+        if(antallforsok<6 && !(temp.equals(ord))) {
         b.setBackgroundColor(Color.RED);
-        for (int i=0; i<ord.size(); i++) {
-            if(c.equals(ord.get(i))) {
-                b.setBackgroundColor(Color.GREEN);
-                temp.set(i,c);
+            for (int i = 0; i < ord.size(); i++) {
+                if (c.equals(ord.get(i))) {
+                    loop = false;
+                    b.setBackgroundColor(Color.GREEN);
+                    temp.set(i, c);
+                }
             }
+            counter(loop);
+            tekst1.setText(Arrays.toString(temp.toArray()));
+            tekst2.setText(String.valueOf(antallforsok));
+            b.setEnabled(false);
         }
-        tekst1.setText(Arrays.toString(temp.toArray()));
-        tekst2.setText(String.valueOf(antallforsok));
-        b.setEnabled(false);
     }
 
 
@@ -96,7 +120,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         Button b = (Button) view;
         String stringTemp = (String) b.getText();
+        loop = true;
         gameLogic(ordArray, temp,Character.valueOf(stringTemp.charAt(0)), b);
-
+        if(antallforsok == 6) {
+            tekst.setText("TAPTE");
+        } else if(temp.equals(ordArray)) {
+            tekst.setText("VANT");
+        }
     }
 }
